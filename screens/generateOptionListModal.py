0b54912e30b5cc9fbaa02779187caccce4b32a49
Alexandre Borges <aborges0501@gmail.com>
2026-03-23 20:15:58 +0000

    Clean up - GenerateOptionListModal
    Forgot to remove bindings that shouldn't be there

--- screens/generateOptionListModal.py
+++ screens/generateOptionListModal.py
@@ -1,16 +1,10 @@
 from textual.app import ComposeResult
-from textual.binding import Binding
 from textual.screen import ModalScreen
 from components.vimMotionOptionList import VimMotionOptionList
 
 
 class GenerateOptionListModal(ModalScreen):
     ESCAPE_TO_MINIMIZE = True
-
-    BINDINGS: list[Binding] = [
-        Binding("j", "cursor_down", "Down", True),
-        Binding("k", "cursor_up", "Up", True),
-    ]
 
     def compose(self) -> ComposeResult:
         optionList = VimMotionOptionList(
